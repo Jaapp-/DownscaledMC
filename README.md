410a98f90bbc8ef4c1d768274bd32ff82233b1d1
Jaap Heijligers <jaap@heijligers.me>
2020-03-27 18:00:12 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# DownscaledMC
+Generated downscaled Minecraft block textures.
+
+## Screenshots
+[BSL Shaders](https://bitslablab.com/) were used in these screenshots.
+
+### 1x1
+![1x1](img/1x1.png)
+
+### 2x2
+![2x2](img/2x2.png)
+
+### 4x4
+![4x4](img/4x4.png)
+
+### 8x8
+![8x8](img/8x8.png)
+
+### 16x16
+![16x16](img/16x16.png)
+
+## Building
+**Requirements**
+- Linux
+- Minecraft at `~/.minecraft` with the selected version installed
+- ImageMagick
+
+**Generate the resource packs**
+1. Set `VERSION=` to extract sources from the desired Minecraft version
+2. Run generate.sh
+3. Resource packs are now installed to `~/.minecraft/resourcepacks/`, they can also be found in `dist/`
+
---
+++ generate.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -e
+VERSION=1.15.2
+
+jar_file=~/.minecraft/versions/$VERSION/$VERSION.jar
+[[ -f $jar_file ]] || {
+  echo "$jar_file not found."
+  exit 1
+}
+
+[[ -e build ]] && rm -r build
+[[ -e dist ]] && rm -r dist
+mkdir dist build
+echo "Unzipping minecraft jar"
+unzip -q ~/.minecraft/versions/$VERSION/$VERSION.jar -d build/minecraft/
+for size in 1 2 4 8 16; do
+  name=${size}x${size}
+  base_dir="build/$name"
+  echo "Creating $name texture pack"
+  [[ -e $base_dir ]] && rm -r $base_dir
+  for p in "assets/minecraft/textures/block" "assets/minecraft/blockstates" "assets/minecraft/models/block" "assets/minecraft/models/item"; do
+    mkdir -p $base_dir/$p
+    cp build/minecraft/$p/* $base_dir/$p/
+  done
+  FILES=$(file $base_dir/assets/minecraft/textures/block/* | grep "PNG image data, 16 x 16, 8-bit/color RGBA, non-interlaced" | cut -d':' -f 1)
+  mogrify -filter point -resize ${size}x${size} $FILES
+  sed "s/%name%/$name/" pack.mcmeta >$base_dir/pack.mcmeta
+  cd $base_dir
+  zip -qr "../../dist/DownsizedMC_$name.zip" .
+  cd ../..
+  rm -r $base_dir
+done
+[[ -d ~/.minecraft/resourcepacks ]] && {
+  echo "Installing $(ls dist/*.zip) to ~/.minecraft/resourcepacks/"
+  cp dist/*.zip ~/.minecraft/resourcepacks/
+}
+echo Done
